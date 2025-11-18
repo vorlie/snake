4751d46734c3fcdb57c77c28a67c6bbfddc58b53
vorlie <vorlie0961@gmail.com>
2025-11-18 23:35:36 +0100

    chore: bump display version to v1.1.1

--- src/app.py
+++ src/app.py
@@ -90,7 +90,7 @@ class SnakeGameApp:
             flags |= pygame.FULLSCREEN
             
         self.screen = pygame.display.set_mode(self.settings["resolution"], flags)
-        pygame.display.set_caption("Snake Shader v1.1.0")
+        pygame.display.set_caption("Snake Shader v1.1.1")
         
         self.ctx = moderngl.create_context()
         self.ctx.enable(moderngl.BLEND)
